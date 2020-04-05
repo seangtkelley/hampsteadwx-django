a588bf3e92b52d10c5f0189fc61cefb3352905ca
Sean Kelley <seangtkelley@gmail.com>
2020-04-05 13:25:33 -0400

    added app files

--- boilerplate/settings.py
+++ boilerplate/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
