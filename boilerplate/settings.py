815464b57c5881ed92e5df7cdea14ec9c00d9e93
Sean Kelley <seangtkelley@gmail.com>
2020-04-11 13:06:42 -0400

    change app subfolder name; started model work

--- boilerplate/settings.py
+++ boilerplate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
+    'api.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('app.urls')),
+    # public site
+    
+
+    # autogen
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
