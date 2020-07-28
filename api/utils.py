0de4e4ab83e3718da3186a249447396251abc408
Sean Kelley <seangtkelley@gmail.com>
2020-07-28 10:32:46 -0400

    fixed grtr vals

--- api/utils.py
+++ api/utils.py
@@ -213,14 +213,14 @@ def calc_general_summary(df):
         'max_temp': Decimal(df.max_temp.max()),
         'max_temp_dates': list(df[df.max_temp == df.max_temp.max()].date),
         'max_temp_avg': Decimal(df.max_temp.mean()),
-        'max_temp_grtr90_count': len(df[df.max_temp > 90]),
-        'max_temp_less32_count': len(df[df.max_temp < 32]),
+        'max_temp_grtr90_count': len(df[df.max_temp >= 90]),
+        'max_temp_less32_count': len(df[df.max_temp <= 32]),
 
         'min_temp': Decimal(df.min_temp.min()),
         'min_temp_dates': list(df[df.min_temp == df.min_temp.min()].date),
         'min_temp_avg': Decimal(df.min_temp.mean()),
-        'min_temp_less32_count': len(df[df.min_temp < 32]),
-        'min_temp_less0_count': len(df[df.min_temp < 0]),
+        'min_temp_less32_count': len(df[df.min_temp <= 32]),
+        'min_temp_less0_count': len(df[df.min_temp <= 0]),
 
         'avg_temp': Decimal(df[['max_temp', 'min_temp']].mean(axis=1).mean()),
 
@@ -232,30 +232,30 @@ def calc_general_summary(df):
         
         'grtst_precip': Decimal(df.precip.max()),
         'grtst_precip_dates': [] if df.precip.max() == 0 else list(df[df.precip == df.precip.max()].date),
-        'precip_grtrT': len(df[df.precip > TRACE_VAL]), # trace (T)
-        'precip_grtr01': len(df[df.precip > 0.01]), # 01 = 0.01"
-        'precip_grtr10': len(df[df.precip > 0.10]), # 10 = 0.10"
-        'precip_grtr50': len(df[df.precip > 0.50]),
-        'precip_grtr100': len(df[df.precip > 1.00]),
+        'precip_grtrT': len(df[df.precip >= TRACE_VAL]), # trace (T)
+        'precip_grtr01': len(df[df.precip >= 0.01]), # 01 = 0.01"
+        'precip_grtr10': len(df[df.precip >= 0.10]), # 10 = 0.10"
+        'precip_grtr50': len(df[df.precip >= 0.50]),
+        'precip_grtr100': len(df[df.precip >= 1.00]),
 
         # snowfall and snowdepth fields
         'sf': TRACE_VAL if Decimal(df.snowfall.max()) == TRACE_VAL else Decimal(sum(df[df.snowfall != TRACE_VAL].snowfall)),
 
         'grtst_sf': Decimal(df.snowfall.max()),
         'grtst_sf_dates': [] if df.snowfall.max() == 0 else list(df[df.snowfall == df.snowfall.max()].date),
-        'sf_grtrT': len(df[df.snowfall > TRACE_VAL]),
-        'sf_grtr1': len(df[df.snowfall > 1]), # in.
-        'sf_grtr3': len(df[df.snowfall > 3]),
-        'sf_grtr6': len(df[df.snowfall > 6]),
-        'sf_grtr12': len(df[df.snowfall > 12]),
-        'sf_grtr18': len(df[df.snowfall > 18]),
+        'sf_grtrT': len(df[df.snowfall >= TRACE_VAL]),
+        'sf_grtr1': len(df[df.snowfall >= 1]), # in.
+        'sf_grtr3': len(df[df.snowfall >= 3]),
+        'sf_grtr6': len(df[df.snowfall >= 6]),
+        'sf_grtr12': len(df[df.snowfall >= 12]),
+        'sf_grtr18': len(df[df.snowfall >= 18]),
 
         'grtst_sd': Decimal(df.snowdepth.max()),
         'grtst_sd_dates': [] if df.snowdepth.max() == 0 else list(df[df.snowdepth == df.snowdepth.max()].date),
-        'sd_grtrT': len(df[df.snowdepth > TRACE_VAL]),
-        'sd_grtr1': len(df[df.snowdepth > 1]), # in.
-        'sd_grtr3': len(df[df.snowdepth > 3]),
-        'sd_grtr6': len(df[df.snowdepth > 6]),
-        'sd_grtr12': len(df[df.snowdepth > 12]),
-        'sd_grtr18': len(df[df.snowdepth > 18])
+        'sd_grtrT': len(df[df.snowdepth >= TRACE_VAL]),
+        'sd_grtr1': len(df[df.snowdepth >= 1]), # in.
+        'sd_grtr3': len(df[df.snowdepth >= 3]),
+        'sd_grtr6': len(df[df.snowdepth >= 6]),
+        'sd_grtr12': len(df[df.snowdepth >= 12]),
+        'sd_grtr18': len(df[df.snowdepth >= 18])
     }
